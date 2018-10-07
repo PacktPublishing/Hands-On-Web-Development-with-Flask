197b7a0801b3b2ef6b19ff56d59fa21da768cfd0
Daniel Gaspar <danielvazgaspar@gmail.com>
2018-10-07 09:16:17 +0100

    New, deploy on Heroku and AWS elastic beanstalk

--- Chapter-13/celery_runner.py
+++ Chapter-13/celery_runner.py
@@ -8,7 +8,7 @@ def make_celery(app):
     celery = Celery(
         app.import_name,
         broker=app.config['CELERY_BROKER_URL'],
-        backend=app.config['CELERY_BACKEND_URL']
+        result_backend=app.config['CELERY_BACKEND_URL']
     )
     celery.conf.update(app.config)
     TaskBase = celery.Task
