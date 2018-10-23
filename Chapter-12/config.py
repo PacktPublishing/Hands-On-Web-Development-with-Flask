d116c24a4e663f3c8c2ebc44a5a1d3231a386751
Daniel Gaspar <danielvazgaspar@gmail.com>
2018-10-23 21:35:02 +0100

    New, added Chapter-12

--- Chapter-12/config.py
+++ Chapter-12/config.py
@@ -49,5 +49,22 @@ class DevConfig(Config):
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
-
     CACHE_TYPE = 'simple'
+
+
+class TestConfig(Config):
+
+    DEBUG = True
+    DEBUG_TB_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CACHE_TYPE = 'null'
+    WTF_CSRF_ENABLED = False
+
+    CELERY_BROKER_URL = "amqp://guest:guest@localhost:5672//"
+    CELERY_BACKEND_URL = "amqp://guest:guest@localhost:5672//"
+
+    MAIL_SERVER = 'localhost'
+    MAIL_PORT = 25
+    MAIL_USERNAME = 'username'
+    MAIL_PASSWORD = 'password'
